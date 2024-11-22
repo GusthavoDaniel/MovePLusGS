9d5233dec1e78283b322e8d069927e81807a506d
GusthavoDaniel <167530772+GusthavoDaniel@users.noreply.github.com>
2024-11-22 19:25:35 -0300

    foi adiciona use client

--- moveplus/src/app/avaliacao-sustentavel/page.tsx
+++ moveplus/src/app/avaliacao-sustentavel/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState } from "react";
 import { Container, Typography, Button, RadioGroup, FormControlLabel, Radio, Box } from "@mui/material";
 
--- moveplus/src/app/chatbot/page.tsx
+++ moveplus/src/app/chatbot/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState } from "react";
 import { Container, Typography, TextField, Button, Box } from "@mui/material";
 
